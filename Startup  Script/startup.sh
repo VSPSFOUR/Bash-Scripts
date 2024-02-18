ed57d91b269fd3aad5dfaf7eb77bc780f7c80c61
Vyasa Aiyer <vyasa.aiyer@gmail.com>
2024-02-18 16:25:12 +0200

    Switch Browers for ChatGPT

--- Startup  Script/startup.sh
+++ Startup  Script/startup.sh
@@ -7,8 +7,9 @@ nohup obsidian&
 code &
 firefox https://outlook.office.com/mail/&
 firefox https://learn2024.ukzn.ac.za/&
-firefox https://chat.openai.com/&
+# firefox https://chat.openai.com/&
 firefox https://github.com/&
 firefox https://calendar.google.com/calendar/u/0/r/day&
+google-chrome-stable https://chat.openai.com/&
 
 echo Done
