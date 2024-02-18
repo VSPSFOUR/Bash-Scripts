fa56572905ca5a4d5a0a335bd161a5b1d380e1cb
Vyasa Aiyer <vyasa.aiyer@gmail.com>
2024-02-18 16:17:00 +0200

    Altered for 2024

--- Startup  Script/startup.sh
+++ Startup  Script/startup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 date 
-echo Welcome Vyasa
+echo Welcome, Vyasa.
 # curl wttr.in
 echo Opening Applications...
 nohup obsidian&
-code Projects/&
+code &
 firefox https://outlook.office.com/mail/&
-firefox https://learn2023.ukzn.ac.za/&
+firefox https://learn2024.ukzn.ac.za/&
 firefox https://chat.openai.com/&
 firefox https://github.com/&
 firefox https://calendar.google.com/calendar/u/0/r/day&
